9413da2d9698bf6a4080c2986341e2872e4fcc36
while519 <wtyj891@gmail.com>
2016-01-22 12:01:58 +0000

    adding a main file as well as making the code only saving the last 10/100 model

---
+++ wordnet/main.py
@@ -0,0 +1,8 @@
+from wordnet_train import *
+
+# training the model
+launch(test_all=10, totepochs=1000, neval=1000, marge=1., nbatches=100,
+            alpha=1., beta=0.01, lremb=0.002, lrparam=0.002, ndim=20, seed=132)
+
+# evaluation for the model
+wordnet_evaluation
