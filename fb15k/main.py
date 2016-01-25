a2081bd2c96eb193e60b669f174775ad667d8e66
while519 <wtyj891@gmail.com>
2016-01-25 11:06:19 +0000

    bug fixed

--- fb15k/main.py
+++ fb15k/main.py
@@ -1,4 +1,5 @@
 from fb15k_train import *
+import fb15k_evaluation
 
 # Training the model
 launch(test_all=10, totepochs=500, neval=1, marge=0.3, nbatches=100, alpha=1., beta=0.01, lremb=0.002, lrparam=0.002)
