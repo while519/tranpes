26335d8d5a3a27989aa230c7a223ffac43165536
while519 <wtyj891@gmail.com>
2016-01-25 10:45:58 +0000

    bug fixed

--- wordnet/main.py
+++ wordnet/main.py
@@ -5,5 +5,5 @@ import wordnet_evaluation
 launch(test_all=10, totepochs=1000, neval=1000, marge=1., nbatches=100,
             alpha=1., beta=0.01, lremb=0.002, lrparam=0.002, ndim=20, seed=132)
 
-evaluation for the model
+# evaluation for the model
 wordnet_evaluation
